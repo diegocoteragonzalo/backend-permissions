99dee746d27a5512ebf5ca65497b7a013ba991c8
Diego <diegocotera@gmail.com>
2024-08-31 14:04:36 +0200

    5.11.3 Creación de un token JWT

--- app.js
+++ app.js
@@ -2,14 +2,19 @@ const express = require("express")
 const app = express()
 let port = 8081
 
+
 app.use(express.json())
 
 
 let routerPermissions = require("./routers/routerPermissions")
 let routerUsers = require("./routers/routerUsers")
+let routerLogin = require("./routers/routerLogin")
+
 
 app.use("/permissions", routerPermissions)
 app.use("/users", routerUsers)
+app.use("/login", routerLogin)
+
 
 app.listen(port, () => {
 	console.log("http://localhost:" + port)
